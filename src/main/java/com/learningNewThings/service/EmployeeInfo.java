3b4f21b3c78e180fd45ef1b2bdbba88d37b2afa7
Gyanendra-Yadav <gyanendra.yadav@deltatech.gg>
2023-08-05 12:51:34 +0530

    Started working on business layer of the Application

---
+++ src/main/java/com/learningNewThings/service/EmployeeInfo.java
@@ -0,0 +1,14 @@
+package com.learningNewThings.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created By:  Gyanendra_Yadav
+ * on 2023-08-05,Aug,2023
+ * in Project: LearningSpringBoot
+ */
+
+@Service
+public class EmployeeInfo {
+
+}
